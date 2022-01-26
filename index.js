1c5ba0185dc2218937d9edfbdc5ae8015c32f166
zfixler <zakfixler@gmail.com>
2022-01-26 06:48:34 -0500

    app_key test

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000
 app.post('/', async (req, res) => {
   const { APP_KEY } = process.env;
   const ACTION_KEY  = req.headers.authorization.split(" ")[1];
-  console.log(req.headers.authorization.split(" "), ACTION_KEY)
+  console.log(APP_KEY, ACTION_KEY)
 try {
   if(ACTION_KEY === APP_KEY){
     await scrapeRpcna().catch(e => console.log(e))
