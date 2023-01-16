1ba471fd5a46c3c8a7124a6deb87fbf0bbdefb67
Zak Fixler <zakfixler@gmail.com>
2023-01-16 11:20:09 -0500

    test deploy

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -6,5 +6,5 @@ const { join } = require('path');
 module.exports = {
 	// Changes the cache location for Puppeteer.
 	// cacheDirectory: join(__dirname, 'project/src/', '.cache', 'puppeteer'),
-	// cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+	cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
