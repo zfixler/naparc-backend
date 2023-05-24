d4629d548ee5a5f2326d169060f5047dd4300c82
Zak Fixler <zakfixler@gmail.com>
2023-05-24 07:58:14 -0400

    Improve scraper results logging

--- index.js
+++ index.js
@@ -30,28 +30,68 @@ app.post('/', async (req, res) => {
 			let results = [];
 
 			const opc = await runScrape().catch((e) => console.log(e));
-			if (opc) results = [...results, ...opc.results];
+			if (opc) {
+				results = [...results, ...opc.results];
+				console.log(`OPC scraper returned ${ opc.results.length } results.`)
+			} else {
+				console.log('OPC scraper failed.')
+			}
 
 			const rpcna = await scrapeRpcna().catch((e) => console.log(e));
-			if (rpcna) results = [...results, ...rpcna.results];
+			if (rpcna) {
+				results = [...results, ...rpcna.results];
+				console.log(`RPCNA scraper returned ${ rpcna.results.length } results.`)
+			} else {
+				console.log('RPCNA scraper failed.')
+			}
 
 			const arp = await scrapeArp().catch((e) => console.log(e));
-			if (arp) results = [...results, ...arp.results];
+			if (arp) {
+				results = [...results, ...arp.results];
+				console.log(`ARP scraper returned ${ arp.results.length } results.`)
+			} else {
+				console.log('ARP scraper failed.')
+			}
 
 			const frcna = await scrapeFrcna().catch((e) => console.log(e));
-			if (frcna) results = [...results, ...frcna.results];
+			if (frcna) {
+				results = [...results, ...frcna.results];
+				console.log(`FRCNA scraper returned ${ frcna.results.length } results.`)
+			} else {
+				console.log('FRCNA scraper failed.')
+			}
 
 			const hrc = await scrapeHrc().catch((e) => console.log(e));
-			if (hrc) results = [...results, ...hrc.results];
+			if (hrc) {
+				results = [...results, ...hrc.results];
+				console.log(`HRC scraper returned ${ hrc.results.length } results.`)
+			} else {
+				console.log('HRC scraper failed.')
+			}
 
 			const pca = await scrapePca().catch((e) => console.log(e));
-			if (pca) results = [...results, ...pca.results];
+			if (pca) {
+				results = [...results, ...pca.results];
+				console.log(`PCA scraper returned ${ pca.results.length } results.`)
+			} else {
+				console.log('PCA scraper failed.')
+			}
 
 			const urcna = await scrapeUrcna().catch((e) => console.log(e));
-			if (urcna) results = [...results, ...urcna.results];
+			if (urcna) {
+				results = [...results, ...urcna.results];
+				console.log(`URCNA scraper returned ${ urcna.results.length } results.`)
+			} else {
+				console.log('URCNA scraper failed.')
+			}
 
 			const rcus = await scrapeRcus().catch((e) => console.log(e));
-			if (rcus) results = [...results, ...rcus.results];
+			if (rcus) {
+				results = [...results, ...rcus.results];
+				console.log(`RCUS scraper returned ${ rcus.results.length } results.`)
+			} else {
+				console.log('RCUS scraper failed.')
+			}
 			
 			console.log(`All scrapers completed with ${ results.length } results...`);
 			await db.updateDb(results).catch(e => console.log(e));
